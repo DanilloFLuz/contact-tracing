b01b36f7214319cfdc1e2053190a0a4104814d60
DanilloFLuz <danillo_fel98@hotmail.com>
2024-09-05 18:10:32 -0300

    Commit antes que eu esqueça

--- force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
+++ force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
@@ -6,8 +6,9 @@
             {label: "Name", fieldName: "Name", type: "text"},
             {label: "Phone", fieldName: "Mobile__c", type: "text"},
             {label: "Token", fieldName: "Token__c", type: "text"},
-            {label: "Health Status", fieldName: "Health_Status__c ", type: "text"},
-            {label: "Status Update Date", fieldName: "Status_Update_Date__c ", type: "date"},
+            {label: "Health Status", fieldName: "Health_Status__c", type: "text"},
+            {label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date"},
+            {label: "View/Update", type: 'button', initialWidth: 135, typeAttributes: { label: 'View Details', name: 'view_details', title: 'Click to View Details'}},
         ] : 
         [
             {label: "Name", fieldName: "Name", type: "text"},
@@ -16,9 +17,39 @@
             {label: "Address", fieldName: "Address__c", type: "text"},
             {label: "Red Score", fieldName: "Red_Score__c", type: "number"},
             {label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date"},
+            {label: "View/Update", type: 'button', initialWidth: 135, typeAttributes: { label: 'View Details', name: 'view_details', title: 'Click to View Details'}},
         ];
         
         helper.fetchResult(component, scope);
         component.set("v.columns", columns);
+    },
+
+    handleKeyUp : function (cmp, evt, helper) {
+        var isEnterKey = evt.keyCode === 13;
+        var queryTerm = cmp.find('enter-search').get('v.value');
+        if(!queryTerm){
+            cmp.set("v.data", cmp.get("v.initialResponse"));
+        }
+        if (isEnterKey) {
+            cmp.set('v.issearching', true);
+            helper.searchRecord(cmp, queryTerm);
+        }
+    },
+
+    handleRowAction: function (component, event, helper) {
+        const action = event.getParam("action");
+        const row = event.getParam("row");
+        const scope = component.get("v.scope");
+
+        switch (action.name) {
+            case "view_details":
+                const appEvent = scope === "person" ? $A.get("e.c:CTPersonSelectEvent") : $A.get("e.c:CTLocationSelectEvent");
+                appEvent.setParams({
+                    recordId: row.Id,
+                    status: scope === "person" ? row.Health_Status__c : row.Status__c
+                });
+                appEvent.fire();
+                break;
+        }
     }
 })
--- force-app/main/default/aura/CTRecentChanges/CTRecentChangesHelper.js
+++ force-app/main/default/aura/CTRecentChanges/CTRecentChangesHelper.js
@@ -1,11 +1,31 @@
 ({
     fetchResult : function(component, scope) {
-        let action = scope === 'person' ? component.get("c.getRecentPersonHelthChanges") : component.get("c.getRecentLocationHealthChanges");
+        const action = scope === 'person' ? component.get("c.getRecentPersonHelthChanges") : component.get("c.getRecentLocationHealthChanges");
         action.setCallback(this, function (response){
             const state = response.getState();
             if(state === 'SUCCESS'){
                 const resp = response.getReturnValue();
                 component.set("v.data", resp);
+                component.set("v.initialResponse", resp);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    searchRecord : function (component, queryTerm) {
+        const action = component.get("v.scope") === 'person' ? component.get("c.searchPeople") : component.get("c.searchLocations");
+        action.setParams({
+            "searchTerm": queryTerm
+        });
+
+        action.setCallback(this, function (response){
+            const state = response.getState();
+            if(state === 'SUCCESS'){
+                const resp = response.getReturnValue();
+                if(resp && resp.length > 0){
+                    component.set("v.data", resp);
+                }
+                component.set('v.issearching', false);
             }
         });
         $A.enqueueAction(action);
